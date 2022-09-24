c4e69c7dc32175d415030588291f808674073bfe
lenon nascimento <lenoninformatica@gmail.com>
2022-09-23 22:36:44 -0300

    :construction: Requisito 4 feito

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Title from './Title';
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div>
         <p data-testid="solar-system" />
+        <Title headline="Planetas" />
       </div>
     );
   }
