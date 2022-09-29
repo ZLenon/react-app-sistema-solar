c6f88111865bf2ab7c4f8fbd80f7f9d34e66b048
lenon nascimento <lenoninformatica@gmail.com>
2022-09-28 21:49:46 -0300

    :goal_net: Erros de lint do componente MissionCard :ok_hand:

--- src/components/MissionCard.js
+++ src/components/MissionCard.js
@@ -1,16 +1,36 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MissionCard extends React.Component {
   render() {
-    const { name, year, country, destination } = this.props;
+    const {
+      name,
+      year,
+      country,
+      destination,
+    } = this.props;
     return (
       <div data-testid="mission-card">
-        <div data-testid="mission-name">{name}</div>
-        <div data-testid="mission-year">{year}</div>
-        <div data-testid="mission-country">{country}</div>
-        <div data-testid="mission-destination">{destination}</div>
+        <div data-testid="mission-name">
+          {name}
+        </div>
+        <div data-testid="mission-year">
+          {year}
+        </div>
+        <div data-testid="mission-country">
+          {country}
+        </div>
+        <div data-testid="mission-destination">
+          {destination}
+        </div>
       </div>
     );
   }
 }
+MissionCard.propTypes = {
+  name: PropTypes.string.isRequired,
+  year: PropTypes.number.isRequired,
+  country: PropTypes.string.isRequired,
+  destination: PropTypes.string.isRequired,
+};
 export default MissionCard;
