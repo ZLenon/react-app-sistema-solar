7a4a3614e78d6da5c5aa56fcfc127cb8c7b861dc
lenon nascimento <lenoninformatica@gmail.com>
2022-09-28 22:27:20 -0300

    :goal_net: Erros de lints resolvidos :ok_hand:

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -3,18 +3,19 @@ import PropTypes from 'prop-types';
 
 class PlanetCard extends React.Component {
   render() {
-    const { planetName, planetImage } = this.props;
+    const {
+      planetName,
+      planetImage,
+    } = this.props;
     return (
       <div data-testid="planet-card">
-        <p data-testid="planet-name">
-          { planetName }
-        </p>
+        <p data-testid="planet-name">{ planetName }</p>
         <img src={ planetImage } alt={ `Planeta ${planetName}` } />
       </div>
     );
   }
 }
-PlanetCard.protoType = {
+PlanetCard.propTypes = {
   planetName: PropTypes.string.isRequired,
   planetImage: PropTypes.string.isRequired,
 };
